6943b7b403b6984ab6238d782c5e11483fb29387
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-02-19 23:32:49 +0100

    Update to v0.1.1

--- mix.exs
+++ mix.exs
@@ -2,18 +2,18 @@ defmodule Blacksmith.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :blacksmith,
-      version: "0.1.0",
-      elixir: "~> 1.0",
-      deps: deps,
-      name: "Blacksmith",
-      source_url: "https://github.com/batate/blacksmith",
-      docs: fn ->
-        {ref, 0} = System.cmd("git", ["rev-parse", "--verify", "--quiet", "HEAD"])
-        [source_ref: ref, main: "README", readme: true]
-      end,
-      description: "Elixir fake data generation for testing and development",
-      package: package]
+    [app: :blacksmith,
+     version: "0.1.1",
+     elixir: "~> 1.0",
+     deps: deps,
+     name: "Blacksmith",
+     source_url: "https://github.com/batate/blacksmith",
+     docs: fn ->
+       {ref, 0} = System.cmd("git", ["rev-parse", "--verify", "--quiet", "HEAD"])
+       [source_ref: ref, main: "README", readme: "README.md"]
+     end,
+     description: "Elixir fake data generation for testing and development",
+     package: package]
   end
 
   def application do
@@ -29,7 +29,9 @@ defmodule Blacksmith.Mixfile do
   end
 
   defp deps do
-    [ {:shouldi, env: :test},
-      {:faker, "~> 0.4.0"} ]
+    [{:faker, "~> 0.4.0"},
+     {:ex_doc, only: :dev},
+     {:earmark, only: :dev},
+     {:shouldi, only: :test}]
   end
 end
